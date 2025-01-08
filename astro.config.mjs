b6d72b5282abad73e24e73f7a609dfbb4d07994c
Juan Hastier <juan.hastier@gmail.com>
2025-01-07 23:16:45 -0500

    Solution to the QR Code Component challenge

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://juanhastier.github.io",
+    base: "qr-code-component",
+});
